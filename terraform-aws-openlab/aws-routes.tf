d1a9aab2317eccdf186055f49c120b147734b45e
Gineesh Madapparambath <net.gini@gmail.com>
2026-07-24 02:29:20 +0800

    Updated

--- terraform-aws-openlab/aws-routes.tf
+++ terraform-aws-openlab/aws-routes.tf
@@ -4,16 +4,3 @@ resource "aws_route" "openlab_public_route" {
   destination_cidr_block = "0.0.0.0/0"                           # Specify the destination CIDR block
   gateway_id             = aws_internet_gateway.openlab_igw.id   # Reference the Internet Gateway ID
 }
-
-# Create Routes in Private Route Tables to NAT Gateway (both use same NAT GW for cost optimization)
-resource "aws_route" "private_route_az1" {
-  route_table_id         = aws_route_table.openlab_rtb_private1.id
-  destination_cidr_block = "0.0.0.0/0"
-  nat_gateway_id         = aws_nat_gateway.nat_gw.id
-}
-
-resource "aws_route" "private_route_az2" {
-  route_table_id         = aws_route_table.openlab_rtb_private2.id
-  destination_cidr_block = "0.0.0.0/0"
-  nat_gateway_id         = aws_nat_gateway.nat_gw.id
-}
